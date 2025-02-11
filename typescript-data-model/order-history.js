db9209a190eee77d148040622b3c24c75628f382
Duncan Harrison <duncanharrison3@gmail.com>
2025-02-11 02:37:45 +0000

    Fixed typescript-data-model exercise so that the interface did not have to change to meet product changes.

--- typescript-data-model/order-history.js
+++ typescript-data-model/order-history.js
@@ -1,9 +1,6 @@
 'use strict';
 const orderHistory = [
   {
-    orderDate: 'August 4, 2020',
-    totalCost: 34.0,
-    shipTo: 'JS Masher',
     deliverDate: 'Aug 8, 2020',
     title: 'Javascript for impatient programmers',
     creator: 'Rauschmayer, Dr. Axel',
@@ -11,9 +8,6 @@ const orderHistory = [
     price: 31.55,
   },
   {
-    orderDate: 'July 19, 2020',
-    totalCost: 44.53,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 20, 2020',
     title: 'The Timeless Way of Building',
     creator: 'Alexander, Christopher',
@@ -21,9 +15,6 @@ const orderHistory = [
     price: 41.33,
   },
   {
-    orderDate: 'July 4, 2020',
-    totalCost: 17.22,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 7, 2020',
     title:
       'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
@@ -31,16 +22,16 @@ const orderHistory = [
     price: 15.98,
   },
   {
-    orderDate: 'July 3, 2020',
-    totalCost: 138.93,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 5, 2020',
     title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
     returnByDate: 'Aug 4, 2020',
     price: 94.95,
-    title2: 'The Art of Sql',
-    creator2: 'Faroult, Stephane',
-    returnByDate2: 'Aug 4, 2020',
-    price2: 33.99,
+  },
+  {
+    deliverDate: 'Jul 5, 2020',
+    title: 'The Art of Sql',
+    creator: 'Faroult, Stephane',
+    returnByDate: 'Aug 4, 2020',
+    price: 33.99,
   },
 ];
--- typescript-data-model/order-history.ts
+++ typescript-data-model/order-history.ts
@@ -1,7 +1,4 @@
 interface Order {
-  orderDate: string;
-  totalCost: number;
-  shipTo: string;
   deliverDate: string;
   title: string;
   creator?: string;
@@ -12,12 +9,8 @@ interface Order {
   returnByDate2?: string;
   price2?: number;
 }
-
 const orderHistory: Order[] = [
   {
-    orderDate: 'August 4, 2020',
-    totalCost: 34.0,
-    shipTo: 'JS Masher',
     deliverDate: 'Aug 8, 2020',
     title: 'Javascript for impatient programmers',
     creator: 'Rauschmayer, Dr. Axel',
@@ -25,9 +18,6 @@ const orderHistory: Order[] = [
     price: 31.55,
   },
   {
-    orderDate: 'July 19, 2020',
-    totalCost: 44.53,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 20, 2020',
     title: 'The Timeless Way of Building',
     creator: 'Alexander, Christopher',
@@ -35,9 +25,6 @@ const orderHistory: Order[] = [
     price: 41.33,
   },
   {
-    orderDate: 'July 4, 2020',
-    totalCost: 17.22,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 7, 2020',
     title:
       'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
@@ -45,16 +32,16 @@ const orderHistory: Order[] = [
     price: 15.98,
   },
   {
-    orderDate: 'July 3, 2020',
-    totalCost: 138.93,
-    shipTo: 'JS Masher',
     deliverDate: 'Jul 5, 2020',
     title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
     returnByDate: 'Aug 4, 2020',
     price: 94.95,
-    title2: 'The Art of Sql',
-    creator2: 'Faroult, Stephane',
-    returnByDate2: 'Aug 4, 2020',
-    price2: 33.99,
+  },
+  {
+    deliverDate: 'Jul 5, 2020',
+    title: 'The Art of Sql',
+    creator: 'Faroult, Stephane',
+    returnByDate: 'Aug 4, 2020',
+    price: 33.99,
   },
 ];
