8b1caca16ffe890c700743d9391e0245c2bb3c26
Duncan Harrison <duncanharrison3@gmail.com>
2025-02-11 04:32:20 +0000

    Final fix to complete typescript-data-model exercise.

--- typescript-data-model/order-history.ts
+++ typescript-data-model/order-history.ts
@@ -1,43 +1,88 @@
-interface Order {
-  deliverDate: string;
-  title: string;
-  creator?: string;
-  returnByDate: string;
+interface Item {
+  type: string;
+  title?: string;
+  author?: string;
+  name?: string;
+  canReturnUntil: string;
   price: number;
+}
+
+interface Order {
+  orderNumber: string;
+  placedOn: string;
+  total: number;
+  recipient: string;
+  deliveredOn: string;
+  items: Item[];
 }
 const orderHistory: Order[] = [
   {
-    deliverDate: 'Aug 8, 2020',
-    title: 'Javascript for impatient programmers',
-    creator: 'Rauschmayer, Dr. Axel',
-    returnByDate: 'Sep 7, 2020',
-    price: 31.55,
+    orderNumber: '11439416898772232',
+    placedOn: 'August 4, 2020',
+    total: 34,
+    recipient: 'JS Masher',
+    deliveredOn: 'Aug 8, 2020',
+    items: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Raushmayer, Dr. Axel',
+        canReturnUntil: 'Sep 7, 2020',
+        price: 31.55,
+      },
+    ],
   },
   {
-    deliverDate: 'Jul 20, 2020',
-    title: 'The Timeless Way of Building',
-    creator: 'Alexander, Christopher',
-    returnByDate: 'Aug 19, 2020',
-    price: 41.33,
+    orderNumber: '11399842681280257',
+    placedOn: 'July 19, 2020',
+    total: 44.53,
+    recipient: 'JS Masher',
+    deliveredOn: 'Jul 20, 2020',
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        canReturnUntil: 'Aug 19, 2020',
+        price: 41.33,
+      },
+    ],
   },
   {
-    deliverDate: 'Jul 7, 2020',
-    title:
-      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    returnByDate: 'Aug 5, 2020',
-    price: 15.98,
+    orderNumber: '11428755579059409',
+    placedOn: 'July 4, 2020',
+    total: 17.22,
+    recipient: 'JS Masher',
+    deliveredOn: 'Jul 7, 2020',
+    items: [
+      {
+        type: 'hardware',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        canReturnUntil: 'Aug 5, 2020',
+        price: 15.98,
+      },
+    ],
   },
   {
-    deliverDate: 'Jul 5, 2020',
-    title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    returnByDate: 'Aug 4, 2020',
-    price: 94.95,
-  },
-  {
-    deliverDate: 'Jul 5, 2020',
-    title: 'The Art of Sql',
-    creator: 'Faroult, Stephane',
-    returnByDate: 'Aug 4, 2020',
-    price: 33.99,
+    orderNumber: '11328831772648248',
+    placedOn: 'July 3, 2020',
+    total: 138.93,
+    recipient: 'JS Masher',
+    deliveredOn: 'Jul 5, 2020',
+    items: [
+      {
+        type: 'hardware',
+        name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        canReturnUntil: 'Aug 4, 2020',
+        price: 94.95,
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        canReturnUntil: 'Aug 4, 2020',
+        price: 33.99,
+      },
+    ],
   },
 ];
--- typescript-data-model/typescript-data-model-notes.md
+++ typescript-data-model/typescript-data-model-notes.md
@@ -3,7 +3,8 @@
 ## Notes
 
 All student notes should be written here.
-
+*Product info needs title and price. It could have offer.
+*Order should have a product and a means to add a second product. Make the type accommodating to the data and need to add hundreds of products.
 How to write `Code Examples` in markdown
 
 for JS:
