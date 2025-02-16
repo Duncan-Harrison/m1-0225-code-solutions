976a64f557a19f619b18d972bbbaa947f5728181
Duncan Harrison <duncanharrison3@gmail.com>
2025-02-16 07:03:31 +0000

    Completed the typescript-loops exercise.

---
+++ typescript-loops/main.ts
@@ -0,0 +1,86 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            sumArray,
+            reverseString,
+            getKeys,
+            getValues
+ */
+function getNumbersToTen(): number[] {
+  const numbers: number[] = [];
+  let currentNumber: number = 1;
+  while (currentNumber < 11) {
+    numbers.push(currentNumber);
+    currentNumber++;
+  }
+  return numbers;
+}
+
+function getEvenNumbersToTwenty(): number[] {
+  const evenNumbers: number[] = [];
+  let currentNumber: number = 2;
+  while (currentNumber < 21) {
+    evenNumbers.push(currentNumber);
+    currentNumber += 2;
+  }
+  return evenNumbers;
+}
+
+function repeatWord(word: string, times: number): string {
+  let count: number = 1;
+  let repeated: string = '';
+  while (count <= times) {
+    repeated = repeated += word + ' ';
+    count++;
+  }
+  return repeated;
+}
+
+function logEachCharacter(str: string): any {
+  for (let i: number = 0; i < str.length; i++) {
+    console.log(str[i]);
+  }
+}
+
+function doubleAll(numbers: number[]): number[] {
+  const doubled: number[] = [];
+  for (let i: number = 0; i < numbers.length; i++) {
+    const x: number = numbers[i];
+    doubled.push(x * 2);
+  }
+  return doubled;
+}
+
+function sumArray(numbers: number[]): number {
+  let sum: number = 0;
+  for (const i of numbers) {
+    sum = sum += i;
+  }
+  return sum;
+}
+
+function reverseString(str: string): string {
+  let reversed: string = '';
+  for (let i of str) {
+    reversed = i += reversed;
+  }
+  return reversed;
+}
+
+function getKeys(obj: Record<string, unknown>): string[] {
+  const keys: string[] = [];
+  for (const key in obj) {
+    keys.push(`${key}`);
+  }
+  return keys;
+}
+
+function getValues(obj: Record<string, unknown>): string[] {
+  const values: string[] = [];
+  for (const key in obj) {
+    values.push(`${obj[key]}`);
+  }
+  return values;
+}
