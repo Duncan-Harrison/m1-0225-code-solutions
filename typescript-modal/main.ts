c5d821ae870fdcb5c9f26416c4c9889ef6f913c2
Duncan Harrison <duncanharrison3@gmail.com>
2025-03-23 23:31:40 +0000

    Completed typescript-modal exercise

---
+++ typescript-modal/main.ts
@@ -0,0 +1,18 @@
+const $openModal = document.querySelector('.open-modal');
+if (!$openModal) throw new Error('The $openModal query failed');
+const $dismissModal = document.querySelector('.dismiss-modal');
+if (!$dismissModal) throw new Error('The $dismissModal query failed');
+const $dialog = document.querySelector('dialog');
+if (!$dialog) throw new Error('The $dialog query has failed');
+
+$openModal.addEventListener('click', (event: Event) => {
+  if (event) {
+    $dialog.showModal();
+  }
+});
+
+$dismissModal.addEventListener('click', (event: Event) => {
+  if (event) {
+    $dialog.close();
+  }
+});
